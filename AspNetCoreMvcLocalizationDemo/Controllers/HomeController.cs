6c2b1bc4dad3cf9af1e8381dcc5354bfd2d92a8f
Will <doggy.huang@gmail.com>
2024-05-19 00:59:02 +0800

    加入多國語系資源
    1. 設定 DI 與 Middleware ( Program.cs )
    
    2. 建立 Resources 資料夾，所有資源檔都放裡面，想要在地化的檔案，其放置路徑要跟原始檔案一樣，例如：
    /Controllers/HomeController.cs
    其在地化的資源檔就位於：
    /Resources/Controllers/HomeController.*.resx
    如果是 Views 要在地化也是一樣，例如：
    /Views/Home/Privacy.cshtml
    其在地化的資源檔就位於：
    /Resources/Views/Home/Privacy*.resx
    
    3. 建構式注入 IStringLocalizer<T>
    
    4. 透過 HttpContext.Features.Get<IRequestCultureFeature>()!.RequestCulture 可以取得目前選中的 Culture 資訊

--- AspNetCoreMvcLocalizationDemo/Controllers/HomeController.cs
+++ AspNetCoreMvcLocalizationDemo/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using AspNetCoreMvcLocalizationDemo.Models;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Localization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +9,12 @@ namespace AspNetCoreMvcLocalizationDemo.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IStringLocalizer<HomeController> _localizer;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IStringLocalizer<HomeController> localizer)
         {
             _logger = logger;
+            _localizer = localizer;
         }
 
         public IActionResult Index()
@@ -20,6 +24,9 @@ namespace AspNetCoreMvcLocalizationDemo.Controllers
 
         public IActionResult Privacy()
         {
+            var lang = HttpContext.Features.Get<IRequestCultureFeature>()!.RequestCulture.UICulture.Name;
+
+            ViewBag.Message = lang + ": " + _localizer["Hello World"];
             return View();
         }
 
--- AspNetCoreMvcLocalizationDemo/Program.cs
+++ AspNetCoreMvcLocalizationDemo/Program.cs
@@ -1,7 +1,30 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var currentCulture = CultureInfo.CurrentCulture;
+    var supportedCultures = new List<CultureInfo>
+        {
+            new CultureInfo("zh-TW"),
+            new CultureInfo("en"),
+        };
+    options.DefaultRequestCulture = new RequestCulture(currentCulture);
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+
+    // 在回應標頭中加入 Content-Language 標頭，告知用戶端此份 HTTP 內容的語言為何
+    options.ApplyCurrentCultureToResponseHeaders = true;
+});
 
 var app = builder.Build();
 
@@ -15,6 +38,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseRequestLocalization();
 
 app.MapControllerRoute(
     name: "default",
